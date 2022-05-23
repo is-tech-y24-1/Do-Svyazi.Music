bbf529f62d55fd656ed604419a60b8685c07c8c7
Alexander <sashafromlibertalia@gmail.com>
2022-05-23 20:05:25 +0300

    feat(#11): implemented `Song genre` entity

---
+++ Domain/DS.Domain/SongGenre.cs
@@ -0,0 +1,21 @@
+﻿using DS.Common.Exceptions;
+
+namespace DS.Domain;
+
+/**
+ * Describes song genre in Entity Framework.
+ */
+public class SongGenre
+{
+    public SongGenre(string genreName)
+    {
+        if (string.IsNullOrEmpty(genreName))
+            throw new DoSvyaziMusicException("Music genre name can't be empty.");
+        
+        GenreName = genreName;
+        Id = Guid.NewGuid();
+    }
+    
+    public Guid Id { get; }
+    public string GenreName { get; set; }
+}
