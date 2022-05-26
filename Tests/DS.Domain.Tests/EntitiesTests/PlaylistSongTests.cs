3369a6fe906e2b3327f6a736c3d76313fa43a8ba
Andreev Artem <andreev_3731@mail.ru>
2022-05-26 13:35:35 +0300

    test: add tests for PlaylistSong

---
+++ Tests/DS.Domain.Tests/EntitiesTests/PlaylistSongTests.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DS.Common.Exceptions;
+using DS.Domain;
+using NUnit.Framework;
+
+namespace DS.Tests.EntitiesTests;
+
+public class PlaylistSongTests
+{
+    private static List<Song> _songsToTest = new ();
+    
+    [SetUp]
+    public void Setup()
+    {
+        _songsToTest = new List<Song>
+        {
+            new Song("test1", new SongGenre("TestGenre") , new MusicUser(), "_"),
+            new Song("test2", new SongGenre("TestGenre") , new MusicUser(), "_"),
+            new Song("test3", new SongGenre("TestGenre") , new MusicUser(), "_")
+        };
+    }
+
+    [Test]
+    public void AddSongsLastToPlaylistSongLinkedList_SongsAdded()
+    {
+        var start = new PlaylistSong(_songsToTest[0]);
+        start.AddLast(_songsToTest[1]);
+        start.AddLast(_songsToTest[2]);
+
+        CollectionAssert.AreEqual(_songsToTest, start.ToList());
+        
+        foreach (var t in _songsToTest)
+        {
+            Assert.AreEqual(t, start.FindSong(t));
+            Assert.IsTrue(start.Contains(t));
+        }
+    }
+    
+    [Test]
+    public void AddSongsFirstToPlaylistSongLinkedList_SongsAdded()
+    {
+        var start = new PlaylistSong(_songsToTest[2]);
+        start.AddFirst(_songsToTest[1]);
+        start.AddFirst(_songsToTest[0]);
+
+        CollectionAssert.AreEqual(_songsToTest, start.ToList());
+        
+        foreach (var t in _songsToTest)
+        {
+            Assert.AreEqual(t, start.FindSong(t));
+            Assert.IsTrue(start.Contains(t));
+        }
+    }
+
+    [Test]
+    public void InsertSongAfterExisting3_SongInserted()
+    {
+        var start = new PlaylistSong(_songsToTest[0]);
+        start.InsertAfter(_songsToTest[2], _songsToTest[0]);
+        start.InsertAfter(_songsToTest[1], _songsToTest[0]);
+        
+        CollectionAssert.AreEqual(_songsToTest, start.ToList());
+    }
+
+    [Test]
+    public void InsertSongAtUnknownSong_ThrowsException()
+    {
+        var start = new PlaylistSong(_songsToTest[0]);
+        start.AddLast(_songsToTest[1]);
+        var unknownSong = new Song("_", new SongGenre("TestGenre"), new MusicUser(), "_");
+
+        Assert.Throws<EntityNotFoundException>(() =>
+        {
+            start.InsertAfter(_songsToTest[2], unknownSong);
+        });
+    }
+
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(2)]
+    public void RemoveSong_SongRemoves(int positionToRemove)
+    {
+        var start = new PlaylistSong(_songsToTest[0]);
+        start.AddLast(_songsToTest[1]);
+        start.AddLast(_songsToTest[2]);
+
+        start.Remove(_songsToTest[positionToRemove]);
+        _songsToTest.RemoveAt(positionToRemove);
+        
+        CollectionAssert.AreEqual(_songsToTest, start.ToList());
+    }
+
+    [Test]
+    public void RemoveOnlySong_ThrowsException()
+    {
+        var start = new PlaylistSong(_songsToTest[0]);
+        start.AddLast(_songsToTest[1]);
+        
+        start.Remove(_songsToTest[0]);
+
+        Assert.Throws<DoSvyaziMusicException>(() =>
+        {
+            start.Remove(_songsToTest[1]);
+        });
+    }
+}
