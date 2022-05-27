b583f59b936f1c6f6121dee2a73e3bedda6a037e
Alexander <sashafromlibertalia@gmail.com>
2022-05-28 00:23:10 +0300

    fix: update `IEquatable` overrides

--- Domain/DS.Domain/MusicUser.cs
+++ Domain/DS.Domain/MusicUser.cs
@@ -9,23 +9,7 @@ public class MusicUser : IEquatable<MusicUser>
     }
     public Guid Id { get; private init; }
 
-    public bool Equals(MusicUser? other)
-    {
-        if (ReferenceEquals(null, other)) return false;
-        if (ReferenceEquals(this, other)) return true;
-        return Id.Equals(other.Id);
-    }
-
-    public override bool Equals(object? obj)
-    {
-        if (ReferenceEquals(null, obj)) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((MusicUser)obj);
-    }
-
-    public override int GetHashCode()
-    {
-        return Id.GetHashCode();
-    }
+    public bool Equals(MusicUser? other) => other?.Id.Equals(Id) ?? false;
+    public override bool Equals(object? obj) => Equals(obj as MusicUser);
+    public override int GetHashCode() => Id.GetHashCode();
 }
