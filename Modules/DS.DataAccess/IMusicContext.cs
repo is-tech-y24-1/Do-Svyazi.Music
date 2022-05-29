b42616e5034ff7a97874caf5065380b4c3ac345a
Andreev Artem <andreev_3731@mail.ru>
2022-05-29 15:56:47 +0300

    chore: project created + basic implementation

---
+++ Modules/DS.DataAccess/IMusicContext.cs
@@ -0,0 +1,16 @@
+﻿using DS.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DS.DataAccess;
+
+public interface IMusicContext
+{
+    public DbSet<ListeningQueue> ListeningQueues { get; }
+    public DbSet<MediaLibrary> MediaLibraries { get; }
+    public DbSet<MusicUser> MusicUsers { get; }
+    public DbSet<Playlist> Playlists { get; }
+    public DbSet<PlaylistSongNode> PlaylistSongNodes { get; }
+    public DbSet<PlaylistSongs> PlaylistSongs { get; }
+    public DbSet<Song> Songs { get; }
+    public DbSet<SongGenre> SongGenres { get; }
+}
