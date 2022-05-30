78dc89c4ab0433f79c122499f16770d439e6b151
Andreev Artem <andreev_3731@mail.ru>
2022-05-30 15:53:48 +0300

    chore: project to class library, remove comment

--- Modules/DS.DataAccess/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using DS.DataAccess;
-using DS.DataAccess.Context;
-using Microsoft.EntityFrameworkCore;
-
-var options = new DbContextOptionsBuilder<MusicDbContext>().UseSqlite(@"Data Source=D:\Do-Svyazi.Music\test.sqlite").Options;
-IMusicContext test = new MusicDbContext(options);
