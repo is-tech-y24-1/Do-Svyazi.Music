ae00655b033660d7958d737bb17770c8908bacd7
Andreev Artem <andreev_3731@mail.ru>
2022-06-02 15:58:45 +0300

    feat: add playlist controller

--- Source/Application/DS.Application.CQRS/Playlist/Commands/DeleteSongFromPlaylist.cs
+++ Source/Application/DS.Application.CQRS/Playlist/Commands/DeleteSongFromPlaylist.cs
@@ -4,10 +4,10 @@ namespace DS.Application.CQRS.Playlist.Commands;
 
 public static class DeleteSongFromPlaylist
 {
-    public record Command(Guid UserId, Guid PlaylistId, Guid SongId) : IRequest;
+    public record DeleteSongCommand(Guid UserId, Guid PlaylistId, Guid SongId) : IRequest;
 
-    // public class Handler : IRequestHandler<Command>
+    // public class Handler : IRequestHandler<DeleteSongCommand>
     // {
-    //     public async Task<Unit> Handle(Command request, CancellationToken cancellationToken) { }
+    //     public async Task<Unit> Handle(DeleteSongCommand request, CancellationToken cancellationToken) { }
     // }
 }
