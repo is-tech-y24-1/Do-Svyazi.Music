bab92b0a6dc649389780739c2c6f7314fe082649
Andreev Artem <andreev_3731@mail.ru>
2022-06-10 11:29:16 +0300

    feat: add ContentStorageInterface + new exception

---
+++ Source/Modules/DS.DataAccess/ContentStorages/FileSystemStorage.cs
@@ -0,0 +1,24 @@
+﻿namespace DS.DataAccess.ContentStorages;
+
+public class FileSystemStorage : IContentStorage
+{
+    public string GenerateUri()
+    {
+        return Guid.NewGuid().ToString();
+    }
+
+    public void CreateStorageFile(string uri, string fileName, byte[] data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteStorageFile(string uri)
+    {
+        throw new NotImplementedException();
+    }
+
+    public byte[] GetFileData(string uri)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Source/Modules/DS.DataAccess/IContentStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace DS.DataAccess;
+
+public interface IContentStorage
+{
+    string GenerateUri();
+    void CreateStorageFile(string uri, string fileName, byte[] data);
+    void DeleteStorageFile(string uri);
+    byte[] GetFileData(string uri);
+}
