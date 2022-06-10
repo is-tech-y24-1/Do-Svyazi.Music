c2aa8e0a84eb47f5ede56d0589d4f3d2f19967dc
Andreev Artem <andreev_3731@mail.ru>
2022-06-10 11:29:16 +0300

    feat: add ContentStorageInterface + new exception

---
+++ Source/Domain/DS.Common/Exceptions/FileNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace DS.Common.Exceptions;
+
+public class FileNotFoundException : DoSvyaziMusicException
+{
+    protected FileNotFoundException()
+    {
+    }
+
+    public FileNotFoundException(string message) 
+        : base(message)
+    {
+    }
+
+    public FileNotFoundException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
