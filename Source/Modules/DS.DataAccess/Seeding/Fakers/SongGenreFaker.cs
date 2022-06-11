22d67a7adb3b374d618b158b218c48ca9f1bb5c2
Andreev Artem <andreev_3731@mail.ru>
2022-06-11 22:39:55 +0300

    feat: add song genre faker

---
+++ Source/Modules/DS.DataAccess/Seeding/Fakers/SongGenreFaker.cs
@@ -0,0 +1,13 @@
+﻿using AutoBogus;
+using DS.Domain;
+
+namespace DS.DataAccess.Seeding.Fakers;
+
+public sealed class SongGenreFaker : AutoFaker<SongGenre>
+{
+    public SongGenreFaker()
+    {
+        RuleFor(e => e.Id, Guid.NewGuid);
+        RuleFor(e => e.Name, f => f.Music.Genre());
+    }
+}
