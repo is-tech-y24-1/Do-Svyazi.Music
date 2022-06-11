683b0e42a23399d86fc4a8e06bfc31c0b9b87c51
Andreev Artem <andreev_3731@mail.ru>
2022-06-11 22:47:00 +0300

    feat: add playlist faker

---
+++ Source/Modules/DS.DataAccess/Seeding/Fakers/PlaylistFaker.cs
@@ -0,0 +1,22 @@
+﻿using AutoBogus;
+using DS.Common.Extensions;
+using DS.Domain;
+
+namespace DS.DataAccess.Seeding.Fakers;
+
+public sealed class PlaylistFaker : AutoFaker<Playlist>
+{
+    public PlaylistFaker(MusicUser author, PlaylistSongs songs)
+    {
+        author.ThrowIfNull();
+        songs.ThrowIfNull();
+        
+        RuleFor(e => e.Id, Guid.NewGuid);
+        RuleFor(e => e.Name, f => f.Lorem.Word());
+        RuleFor(e => e.Author, author);
+        RuleFor(e => e.Songs, songs);
+        RuleFor(e => e.SharedForCommunity, true);
+        RuleFor(e => e.CoverUri, f => f.Image.PicsumUrl());
+        RuleFor(e => e.Description, f => f.Lorem.Paragraph());
+    }
+}
