87420f26c30b76d417ec282a14fe7470928e7abe
Andreev Artem <andreev_3731@mail.ru>
2022-06-11 22:20:13 +0300

    feat: add music user faker

---
+++ Source/Modules/DS.DataAccess/Seeding/Fakers/MusicUserFaker.cs
@@ -0,0 +1,19 @@
+﻿using AutoBogus;
+using DS.Domain;
+
+namespace DS.DataAccess.Seeding.Fakers;
+
+public sealed class MusicUserFaker : AutoFaker<MusicUser>
+{
+    public static readonly MusicUserFaker Instance = new MusicUserFaker();
+
+    private MusicUserFaker()
+    {
+        var id = Guid.NewGuid();
+        RuleFor(e => e.Id, _ => id);
+        RuleFor(e => e.Name, f => f.Internet.UserName());
+        RuleFor(e => e.ProfilePictureUri, f => f.Internet.Avatar());
+        RuleFor(e => e.MediaLibrary, _ => new MediaLibrary(id));
+        RuleFor(e => e.ListeningQueue, _ => new ListeningQueue(id));
+    }
+}
