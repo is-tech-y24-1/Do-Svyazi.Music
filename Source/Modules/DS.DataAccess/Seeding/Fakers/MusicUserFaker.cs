911e32c2d3714192f2a16e035b135cbcf925898b
Andreev Artem <andreev_3731@mail.ru>
2022-06-11 22:37:37 +0300

    feat: add song faker

--- Source/Modules/DS.DataAccess/Seeding/Fakers/MusicUserFaker.cs
+++ Source/Modules/DS.DataAccess/Seeding/Fakers/MusicUserFaker.cs
@@ -5,15 +5,13 @@ namespace DS.DataAccess.Seeding.Fakers;
 
 public sealed class MusicUserFaker : AutoFaker<MusicUser>
 {
-    public static readonly MusicUserFaker Instance = new MusicUserFaker();
-
-    private MusicUserFaker()
+    public MusicUserFaker()
     {
         var id = Guid.NewGuid();
-        RuleFor(e => e.Id, _ => id);
+        RuleFor(e => e.Id, id);
         RuleFor(e => e.Name, f => f.Internet.UserName());
         RuleFor(e => e.ProfilePictureUri, f => f.Internet.Avatar());
-        RuleFor(e => e.MediaLibrary, _ => new MediaLibrary(id));
-        RuleFor(e => e.ListeningQueue, _ => new ListeningQueue(id));
+        RuleFor(e => e.MediaLibrary, new MediaLibrary(id));
+        RuleFor(e => e.ListeningQueue, new ListeningQueue(id));
     }
 }
---
+++ Source/Modules/DS.DataAccess/Seeding/Fakers/SongFaker.cs
@@ -0,0 +1,18 @@
+﻿using AutoBogus;
+using DS.Domain;
+
+namespace DS.DataAccess.Seeding.Fakers;
+
+public sealed class SongFaker : AutoFaker<Song>
+{
+    public SongFaker(MusicUser author, SongGenre genre)
+    {
+        RuleFor(e => e.Id, Guid.NewGuid());
+        RuleFor(e => e.Name, f => f.Lorem.Word());
+        RuleFor(e => e.ContentUri, f => f.Internet.Url());
+        RuleFor(e => e.CoverUri, f => f.Image.PicsumUrl());
+        RuleFor(e => e.Author, author);
+        RuleFor(e => e.Genre, genre);
+        RuleFor(e => e.SharedForCommunity, true);
+    }
+}
