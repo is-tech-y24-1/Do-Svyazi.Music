7310d40f0445adae42ce95f388cd81e0e8e028d9
Andreev Artem <andreev_3731@mail.ru>
2022-06-12 15:58:13 +0300

    chore: change back to claass library

--- Source/Modules/DS.DataAccess/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using DS.DataAccess.Context;
-using DS.DataAccess.Seeding;
-using Microsoft.EntityFrameworkCore;
-
-DbContextOptions<MusicDbContext> options = new DbContextOptionsBuilder<MusicDbContext>().UseSqlite(@"Data Source=D:\Do-Svyazi.Music\test.sqlite").Options;
-var test = new MusicDbContext(options);
-var seeder = new AutoBogusSeeder();
-seeder.Seed(test);
