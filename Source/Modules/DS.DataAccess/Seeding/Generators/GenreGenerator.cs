e0257c7bcf70298885f854a99e5ac020848812d7
Andreev Artem <andreev_3731@mail.ru>
2022-06-12 12:32:07 +0300

    feat: add genre generator

---
+++ Source/Modules/DS.DataAccess/Seeding/Generators/GenreGenerator.cs
@@ -0,0 +1,12 @@
+﻿using DS.DataAccess.Seeding.Fakers;
+using DS.Domain;
+
+namespace DS.DataAccess.Seeding.Generators;
+
+public class GenreGenerator
+{
+    public IReadOnlyCollection<SongGenre> GenerateSongGenres(int count)
+    {
+        return new SongGenreFaker().Generate(count);
+    }
+}
