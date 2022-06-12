6e2ce9454936ff799795267028e339cbf6450252
Andreev Artem <andreev_3731@mail.ru>
2022-06-12 14:37:07 +0300

    fix: some bugs fixed

--- Source/Modules/DS.DataAccess/Program.cs
+++ Source/Modules/DS.DataAccess/Program.cs
@@ -1,11 +1,9 @@
-﻿using Bogus;
-using DS.DataAccess;
-using DS.DataAccess.Context;
+﻿using DS.DataAccess.Context;
 using DS.DataAccess.Seeding;
-using DS.DataAccess.Seeding.Fakers;
-using DS.Domain;
 using Microsoft.EntityFrameworkCore;
 
-var options = new DbContextOptionsBuilder<MusicDbContext>().UseSqlite(@"Data Source=D:\Do-Svyazi.Music\test.sqlite").Options;
-IMusicContext test = new MusicDbContext(options);
+DbContextOptions<MusicDbContext> options = new DbContextOptionsBuilder<MusicDbContext>().UseSqlite(@"Data Source=D:\Do-Svyazi.Music\test.sqlite").Options;
+var test = new MusicDbContext(options);
+var seeder = new AutoBogusSeeder();
+seeder.Seed(test);
 
