553a8faa835ae8627474638a05193d5790051fb3
Andreev Artem <andreev_3731@mail.ru>
2022-06-12 11:42:44 +0300

    chore: class lib to app for test

---
+++ Source/Modules/DS.DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using Bogus;
+using DS.DataAccess.Seeding.Fakers;
+using DS.Domain;
+
+MusicUser user = (new MusicUserFaker()).Generate();
+Console.WriteLine(user.Id);
+Console.WriteLine(user.Name);
+Console.WriteLine(user.ProfilePictureUri);
+Console.WriteLine(user.MediaLibrary.OwnerId);
+Console.WriteLine(user.ListeningQueue.OwnerId);
+var faker = new Faker();
+Console.WriteLine(faker.Hacker.Noun());
