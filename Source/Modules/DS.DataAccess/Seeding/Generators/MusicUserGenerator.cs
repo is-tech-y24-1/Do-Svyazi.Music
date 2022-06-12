9e7db0cdba27640eb6c6beb60a39923fdd3ead1c
Andreev Artem <andreev_3731@mail.ru>
2022-06-12 12:33:05 +0300

    feat: add music user generator

---
+++ Source/Modules/DS.DataAccess/Seeding/Generators/MusicUserGenerator.cs
@@ -0,0 +1,12 @@
+﻿using DS.DataAccess.Seeding.Fakers;
+using DS.Domain;
+
+namespace DS.DataAccess.Seeding.Generators;
+
+public class MusicUserGenerator
+{
+    public IReadOnlyCollection<MusicUser> GenerateMusicUsers(int count)
+    {
+        return new MusicUserFaker().Generate(count);
+    }
+}
