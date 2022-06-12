444e01da4d26a8742877ac7d7fb05673d0e7782b
Andreev Artem <andreev_3731@mail.ru>
2022-06-12 16:26:43 +0300

    style: some changes

--- Source/Modules/DS.DataAccess/Seeding/Generators/GenreGenerator.cs
+++ Source/Modules/DS.DataAccess/Seeding/Generators/GenreGenerator.cs
@@ -5,8 +5,5 @@ namespace DS.DataAccess.Seeding.Generators;
 
 public class GenreGenerator
 {
-    public static ICollection<SongGenre> GenerateSongGenres(int count)
-    {
-        return new SongGenreFaker().Generate(count);
-    }
+    public static ICollection<SongGenre> GenerateSongGenres(int count) => new SongGenreFaker().Generate(count);
 }
