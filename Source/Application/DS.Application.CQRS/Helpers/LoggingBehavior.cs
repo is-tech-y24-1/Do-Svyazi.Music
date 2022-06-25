28d41529b023d7afb8b257339f106330e6db1f6a
Andreev Artem <andreev_3731@mail.ru>
2022-06-25 13:31:52 +0300

    feat: add logging behavior

---
+++ Source/Application/DS.Application.CQRS/Helpers/LoggingBehavior.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+// ReSharper disable TemplateIsNotCompileTimeConstantProblem
+
+namespace DS.Application.CQRS.Helpers;
+
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>  where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger _logger;
+
+    public LoggingBehavior(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    {
+        string requestName = request.GetType().Name;
+        string requestGuid = Guid.NewGuid().ToString();
+        string requestNameWithGuid = $"{requestName} [{requestGuid}]";
+
+        _logger.LogInformation($"[START] {requestNameWithGuid}");
+        TResponse response;
+
+        try
+        {
+            response = await next();
+        }
+        catch (Exception e)
+        {
+            _logger.LogInformation($"[ERROR] {e.Message} {requestNameWithGuid}");
+            throw;
+        }
+        finally
+        {
+            _logger.LogInformation($"[END] {requestNameWithGuid}");
+        }
+
+        return response;
+    }
+}
