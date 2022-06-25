153583195999761454387dbb28cd47578c3def02
Andreev Artem <andreev_3731@mail.ru>
2022-06-25 15:39:36 +0300

    feat: add validators

---
+++ Validators/MusicUser/AddMusicUserCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using DS.Application.CQRS.MusicUser.Commands;
+using DS.Application.DTO.MusicUser;
+using FluentValidation;
+
+namespace Validators.MusicUser;
+
+public class AddMusicUserCommandValidator : AbstractValidator<AddMusicUser.AddUserCommand>
+{
+    private static readonly string[] PictureFormats = { ".jpg", ".png", ".jpeg", ".heic" };
+    private const int MinNameLength = 5;
+    private const int MaxNameLength = 20;
+    
+    public AddMusicUserCommandValidator()
+    {
+        RuleFor(e => e.MusicUserCreationInfo)
+            .Must(CheckDto);
+    }
+
+    private bool CheckDto(MusicUserCreationInfoDto musicUserCreationInfoDto)
+    {
+        var file = musicUserCreationInfoDto.ProfilePicture;
+        
+        if (file is not null && string.IsNullOrWhiteSpace(file.FileName))
+            if (!PictureFormats.Contains(file.FileName[file.FileName.LastIndexOf('.')..].ToLower()))
+                return false;
+
+        if (musicUserCreationInfoDto.Name.Length <= MinNameLength && musicUserCreationInfoDto.Name.Length >= MaxNameLength)
+            return false;
+        
+        return true;
+    }
+}
---
+++ Validators/Song/CreateNewSongCommandValidator.cs
@@ -0,0 +1,37 @@
+﻿using DS.Application.CQRS.MediaLibrary.Commands;
+using DS.Application.DTO.Song;
+using FluentValidation;
+
+namespace Validators.Song;
+
+public class CreateNewSongCommandValidator : AbstractValidator<CreateNewSong.CreateNewSongCommand>
+{
+    private static readonly string[] PictureFormats = { ".jpg", ".png", ".jpeg", ".heic" };
+    private static readonly string[] SongFormats = { ".wav", ".mp3", ".aac", ".ogg", ".flac", ".aiff" };
+    private const int MinNameLength = 5;
+    private const int MaxNameLength = 20;
+
+    CreateNewSongCommandValidator()
+    {
+        RuleFor(e => e.SongCreationInfo)
+            .Must(CheckDto);
+    }
+
+    private bool CheckDto(SongCreationInfoDto songCreationInfoDto)
+    {
+        var cover = songCreationInfoDto.Cover;
+        var song = songCreationInfoDto.Song;
+        
+        if (cover is not null && string.IsNullOrWhiteSpace(cover.FileName))
+            if (!PictureFormats.Contains(cover.FileName[cover.FileName.LastIndexOf('.')..].ToLower()))
+                return false;
+
+        if (songCreationInfoDto.Name.Length <= MinNameLength && songCreationInfoDto.Name.Length >= MaxNameLength)
+            return false;
+        
+        if (!SongFormats.Contains(song.FileName[song.FileName.LastIndexOf('.')..].ToLower()))
+            return false;
+        
+        return true;
+    }
+}
