ca2a45aab7f40dde4d7142281079ac6386e23e28
Andreev Artem <andreev_3731@mail.ru>
2022-06-25 16:35:18 +0300

    refactor: remove debug logger

--- Source/Application/DS.Application.CQRS/Helpers/ValidationBehavior.cs
+++ Source/Application/DS.Application.CQRS/Helpers/ValidationBehavior.cs
@@ -1,22 +1,18 @@
 ﻿using FluentValidation;
 using MediatR;
-using Microsoft.Extensions.Logging;
 
 namespace DS.Application.CQRS.Helpers;
 
 public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
     private readonly IEnumerable<IValidator<TRequest>> _validators;
-    private readonly ILogger<TRequest> _logger;
-    
-    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators, ILogger<TRequest> logger)
+
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
     {
         _validators = validators;
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
     public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
     {
-        _logger.LogInformation($"Validated!, {_validators.Count()}");
         if (!_validators.Any()) return await next();
         var context = new ValidationContext<TRequest>(request);
             
